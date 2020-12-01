433b838a5096f7cd48729e2e2b3fdc53d27c3258
Rui Tao <rtao02@syr.edu>
2020-12-01 01:20:20 -0500

    My first commit

---
+++ DSNServer/DSNService.cs
@@ -0,0 +1,46 @@
+﻿using System.ServiceModel;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using DSNInterfaces;
+using System.Threading;
+namespace DSNServer
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    //public class Store
+    //{
+    //    public BindingList<SpaceCraft> storedData
+    //    {
+    //        get;
+    //        set;
+    //    }
+    //    public void setValue(BindingList<SpaceCraft> message)
+    //    {
+    //        this.storedData = message;
+    //    }
+    //}
+    public class DSNService : IDSNService
+    {
+
+        //public static Store tempStore { get; set; }
+        public void Connect()
+        {
+            Callback = OperationContext.Current.GetCallbackChannel<ICallbackService>();
+            MessageBox.Show("Connect Success ^^");
+        }
+
+        public static ICallbackService Callback { get; set; }
+
+
+        public void SendMessage(BindingList<SpaceCraft> message)
+        {
+            //MessageBox.Show(message);
+            //send SpaceCraft info from space running program to DSN server(implement later)
+            Thread t = new Thread(() => DSNServerForm.GetNewData(message));
+            t.Start();
+            //DSNServerForm.GetNewData(message);
+            //tempStore = new Store();
+            //tempStore.setValue(message);
+        }
+    }
+}
